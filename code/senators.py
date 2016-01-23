38e6bc00e95351a065dbb935eac0b423c6e013b7
Matthew Brett <matthew.brett@gmail.com>
2016-01-22 22:19:09 -0800

    Update documentation

--- code/senators.py
+++ code/senators.py
@@ -3,8 +3,10 @@ from __future__ import division, print_function
 import json
 from operator import itemgetter
 import re
+import string
 
 import numpy as np
+
 
 with open("senators-list.json") as f:
     senators = json.load(f)
@@ -17,11 +19,47 @@ names = [d["screen_name"] for d in senators["users"]]
 type(timelines)
 len(timelines)
 len(timelines[0])
+len(timelines[1])
+timelines[0]
+timelines[0][0]
+timelines[0][0].keys()
 
-# check who has the most followers
+timelines[0][0]["truncated"]
+timelines[0][0]["text"]
+timelines[0][0]["favorite_count"]
+timelines[0][0]["user"].keys()
+timelines[0][0]["user"]["favourites_count"]
+timelines[0][0]["user"]["followers_count"]
+timelines[0][0]["user"]["friends_count"]
+timelines[0][0]["user"]["description"]
+
+# who has the most followers
+
+# 1. take the 'followers_count' for the first tweet for each senator
+[x["user"]["followers_count"] for x in timelines[0]]
 followers = [t[0]["user"]["followers_count"] for t in timelines]
-zipped = zip(names, followers)
-zipped.sort(key=itemgetter(1))
+
+# 2. connect the names with their follower counts and sort on the counts
+followers = zip(names, followers)
+followers.sort(key=itemgetter(1))
+followers[0]
+followers[-1]
+
+# who is the most favorited
+
+favorite = [t[0]["user"]["favourites_count"] for t in timelines]
+favorite = zip(names, favorite)
+favorite.sort(key=itemgetter(1))
+favorite[0]
+favorite[-1]
+
+# let's start looking at the text in a tweet
+tweet = timelines[0][0]["text"]
+
+# how might we split it up?
+tweet.split()
+re.split('\W', tweet)
+
 
 # get all the tweets and see what words are used
 tweets = [" ".join([tweet["text"] for tweet in tweets])
@@ -30,8 +68,56 @@ words = [w for text in tweets
            for w in re.split('\W', text) if w]
 vocab = sorted(set(words))
 
+# find frequently occuring words, filter out the short ones
+frequent_words = [w for w in vocab if len(w) > 4 and words.count(w) > 30]
+
+
 # construct term-document matrix
 M = np.zeros([len(tweets), len(vocab)])
 for n, tweet in enumerate(tweets):
     for m, term in enumerate(vocab):
         M[n, m] = tweet.count(term)
+
+
+# Step 2: Get additional data for each senator (party, state, etc.)
+# and match up with twitter data
+
+# Step 3: Preliminary text analysis using nltk
+
+tweets_list = [[tweet["text"] for tweet in tl] for tl in timelines]
+
+# http://www.textfixer.com/resources/common-english-words.txt
+with open("common-english-words.txt") as f:
+    stopwords = csv.reader(f).next()
+
+
+def clean(tweet):
+    cleaned_words = [word.lower() for word in tweet.split()
+                     if 'http' not in word
+                     and not word.startswith('@')
+                     and not word.startswith('.@')
+                     and not word.startswith('#')
+                     and word != 'RT']
+    return ' '.join(cleaned_words)
+
+
+def clean(tweet):
+    cleaned_words = [word.lower() for word in tweet.split()
+                     if 'http' not in word
+                     and word.isalpha()
+                     and word != 'RT']
+    return ' '.join(cleaned_words)
+
+
+def all_punct(tweet):
+    return all([c in string.punctuation for c in tweet])
+
+
+def tokenize(tweet):
+    words = [w for w in clean(tweet).split() 
+             if not all_punct(w) and w not in stopwords]
+    return ' '.join(words)
+
+
+tweets_list = [[tokenize(tweet) for tweet in tweets] for tweets in tweets_list]
+tokens_list = [' '.join(tweets) for tweets in tweets_list]
