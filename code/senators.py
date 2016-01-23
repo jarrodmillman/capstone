75f74c0c146a7d03c657306e893a1cde25298824
Jarrod Millman <jarrod.millman@gmail.com>
2016-01-23 01:48:09 -0800

    checkpoint

--- code/senators.py
+++ code/senators.py
@@ -1,23 +1,23 @@
 from __future__ import division, print_function
 
-import csv
 import json
 from operator import itemgetter
 import re
-import string
-
-import numpy as np
 
 
+# 1. Load senators-list.json as senators.
 with open("senators-list.json") as f:
     senators = json.load(f)
 
+# 2. Load timelines.json as timelines.
 with open("timelines.json") as f:
     timelines = json.load(f)
 
-names = [d["screen_name"] for d in senators["users"]]
 
+# 3. What type of datastructure is timelines?
 type(timelines)
+
+# 4. How many timelines are there? What does each timeline correspond to?
 len(timelines)
 len(timelines[0])
 len(timelines[1])
@@ -34,15 +34,21 @@ timelines[0][0]["user"]["followers_count"]
 timelines[0][0]["user"]["friends_count"]
 timelines[0][0]["user"]["description"]
 
-# who has the most followers
+# 5. Make a list of each senator’s screen name using the variable senators.
+names = [d["screen_name"] for d in senators["users"]]
 
-# 1. take the 'followers_count' for the first tweet for each senator
+# 6. Make a list of the number of followers each senator has.
+
+# 6.1. take the 'followers_count' for the first tweet for each senator
 [x["user"]["followers_count"] for x in timelines[0]]
 followers = [t[0]["user"]["followers_count"] for t in timelines]
 
-# 2. connect the names with their follower counts and sort on the counts
+# 6.2. connect the names with their follower counts and sort on the counts
 followers = zip(names, followers)
 followers.sort(key=itemgetter(1))
+
+# 7. What is the screen name of the senator with the largest number of
+# followers?
 followers[0]
 followers[-1]
 
@@ -54,6 +60,9 @@ favorite.sort(key=itemgetter(1))
 favorite[0]
 favorite[-1]
 
+# 8. Make a list called tweets such that each element of the list contains
+# all of one senator’s tweets concatenated as one string.
+
 # let's start looking at the text in a tweet
 tweet = timelines[0][0]["text"]
 tweet
@@ -62,86 +71,16 @@ tweet
 tweet.split()
 re.split('\W', tweet)
 
-
 # get all the tweets and see what words are used
 tweets = [" ".join([tweet["text"] for tweet in tweets])
           for tweets in timelines]
 words = [w for text in tweets
          for w in re.split('\W', text) if w]
+
+# 9. Create a sorted list of all the unique words used in any senators
+# tweets and call it vocab.
 vocab = sorted(set(words))
 
 # find frequently occuring words, filter out the short ones
+
 frequent_words = [w for w in vocab if len(w) > 4 and words.count(w) > 30]
-
-
-# construct term-document matrix
-M = np.zeros([len(tweets), len(vocab)])
-for n, tweet in enumerate(tweets):
-    for m, term in enumerate(vocab):
-        M[n, m] = tweet.count(term)
-
-
-# Step 2: Get additional data for each senator (party, state, etc.)
-# and match up with twitter data
-
-# Step 3: Preliminary text analysis using nltk
-
-tweets_list = [[tweet["text"] for tweet in tl] for tl in timelines]
-
-# http://www.textfixer.com/resources/common-english-words.txt
-with open("common-english-words.txt") as f:
-    stopwords = csv.reader(f).next()
-
-
-# def clean(tweet):
-#    cleaned_words = [word.lower() for word in tweet.split()
-#                     if 'http' not in word
-#                     and word.isalpha()
-#                     and word != 'RT']
-#    return ' '.join(cleaned_words)
-#
-#
-# def clean(tweet):
-#    tweet = tweet.encode('ascii', 'ignore')
-#    cleaned_words = [word.lower() for word in tweet.split()
-#                     if 'http' not in word
-#                     and not word.startswith('@')
-#                     and not word.startswith('.@')
-#                     and not word.startswith('#')
-#                     and word != 'RT']
-#    exclude = set(string.punctuation)
-#    return ' '.join(c for c in cleaned_words if c not in exclude)
-
-
-def clean(tweet):
-    tweet = tweet.encode('ascii', 'ignore')
-    cleaned_words = [word.lower() for word in tweet.split()
-                     if 'http' not in word
-                     and not word.startswith('@')
-                     and not word.startswith('.@')
-                     and not word.startswith('#')
-                     and word != 'RT']
-    return ' '.join(cleaned_words)
-
-
-def all_punct(tweet):
-    # all([c in string.punctuation for c in tweet])
-    return set(tweet).issubset(set(string.punctuation))
-
-
-def remove_punct(word):
-    exclude = set(string.punctuation)
-    return ''.join(c for c in word if c not in exclude)
-
-
-def tokenize(tweet):
-    words = [remove_punct(w) for w in clean(tweet).split()
-             if not all_punct(w) and w not in stopwords]
-    return ' '.join(words)
-
-
-tweets_list = [[tokenize(tweet) for tweet in tweets] for tweets in tweets_list]
-tokens_list = [' '.join(tweets) for tweets in tweets_list]
-
-words = ' '.join(tokens_list).split()
-vocab = sorted(set(words))
