591bcf03677a7e210d069734169395d847c3cfe3
Jarrod Millman <jarrod.millman@gmail.com>
2016-01-23 23:12:36 -0800

    Update documentation

--- code/senators2.py
+++ code/senators2.py
@@ -19,14 +19,14 @@ tweets_list = [[tweet["text"] for tweet in tl] for tl in timelines]
 
 
 def clean(tweet):
-    tweet = tweet.encode('ascii', 'ignore')
+    tweet = tweet.encode("ascii", "ignore")
     cleaned_words = [word.lower() for word in tweet.split() if
-                     'http' not in word and
-                     not word.startswith('@') and
-                     not word.startswith('.@') and
-                     not word.startswith('#') and
-                     word != 'RT']
-    return ' '.join(cleaned_words)
+                     "http" not in word and
+                     not word.startswith("@") and
+                     not word.startswith(".@") and
+                     not word.startswith("#") and
+                     word != "RT"]
+    return " ".join(cleaned_words)
 
 
 def all_punct(tweet):
@@ -36,7 +36,7 @@ def all_punct(tweet):
 
 def remove_punct(word):
     exclude = set(string.punctuation)
-    return ''.join(c for c in word if c not in exclude)
+    return "".join(c for c in word if c not in exclude)
 
 
 # http://www.textfixer.com/resources/common-english-words.txt
@@ -47,14 +47,13 @@ with open("common-english-words.txt") as f:
 def tokenize(tweet):
     words = [remove_punct(w) for w in clean(tweet).split() if
              not all_punct(w) and w not in stopwords]
-    return ' '.join(words)
+    return " ".join(words)
 
 
 tweets_list = [[tokenize(tweet) for tweet in tweets] for tweets in tweets_list]
-tokens_list = [' '.join(tweets) for tweets in tweets_list]
+tokens_list = [" ".join(tweets) for tweets in tweets_list]
 
-words = ' '.join(tokens_list).split()
+words = " ".join(tokens_list).split()
 vocab = sorted(set(words))
 
 frequent_words = [w for w in vocab if len(w) > 4 and words.count(w) > 30]
-
